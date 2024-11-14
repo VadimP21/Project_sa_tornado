5001c987b8024f4d13054c1ad7dfba9182b054ea
vadim-pugachev <vadimp_91@mail.ru>
2024-11-14 17:22:05 +0300

    Связаны модели Product, Category через CategoryProductM2M

--- src/models.py
+++ src/models.py
@@ -1,11 +1,12 @@
 """
 Модели БД
 """
+
 import datetime
 from typing import Annotated
 
-from sqlalchemy import text
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import text, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database import Base
 
@@ -14,6 +15,7 @@ str255 = Annotated[str, 255]
 
 
 class Product(Base):
+    __tablename__ = "product"
     id: Mapped[pk]
     name: Mapped[str255]
     version: Mapped[int] = mapped_column(default=0)
@@ -25,8 +27,30 @@ class Product(Base):
         server_default=text("TIMEZONE('utc',now())"), onupdate=datetime.datetime.utcnow
     )
 
+    categories: Mapped[list["Category"]] = relationship(
+        secondary="category_product_m2m",
+        back_populates="categories"
+    )
 
 class Category(Base):
+    __tablename__ = "category"
     id: Mapped[pk]
     name: Mapped[str255]
     description: Mapped[str255]
+
+    products: Mapped[list["Product"]] = relationship(
+        secondary="category_product_m2m",
+        back_populates="products"
+    )
+
+class CategoryProductM2M(Base):
+    __tablename__ = "category_product_m2m"
+
+    product_id: Mapped[int] = mapped_column(
+        ForeignKey("product.id"),
+        primary_key=True
+    )
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey("category.id"),
+        primary_key=True
+    )

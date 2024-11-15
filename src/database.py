8f2cb09ce7bae5e0817107a2ff3545a9f1f5b9d8
vadim-pugachev <vadimp_91@mail.ru>
2024-11-15 19:03:51 +0300

    fix .env + create .env.example

--- src/database.py
+++ src/database.py
@@ -2,23 +2,22 @@
 Файл с конфигурацией базы данных
 """
 
-import os
-from typing import Annotated
-
+from os import getenv
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import sessionmaker, DeclarativeBase, mapped_column
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 load_dotenv()
-
+"postgresql+psycopg://postgres:111@localhost/test_db"
+db_url = f"{getenv('DB_ENGINE')}://{getenv('DB_USER')}:{getenv('DB_PASSWORD')}@{getenv('DB_HOST')}/{getenv('DB_NAME')}"
 """
 engine - синхронный движок
 async_engine - асинхронный движок
 """
-engine = create_engine(url=os.getenv("DB_POSTGRES_URL"))
+engine = create_engine(url=db_url)
 
-async_engine = create_async_engine(url=os.getenv("DB_POSTGRES_URL"))
+async_engine = create_async_engine(url=db_url)
 
 """
 session_factory - синхронный создатель сеанса работы с БД
@@ -33,5 +32,4 @@ class Base(DeclarativeBase):
     Base - родительский класс для моделей БД в декларативном виде
     """
 
-    type_annotation_map = {}
     pass

91b7f35d91c583fb127e9304cab896b3d8b120f3
vadim-pugachev <vadimp_91@mail.ru>
2024-11-19 23:22:26 +0300

    Fix + Docstrings queries.base

--- src/queries/base.py
+++ src/queries/base.py
@@ -8,8 +8,15 @@ from database import session_factory
 class ProductQueries:
     @staticmethod
     def last_version_product_by_name_query(
-        session: session_factory, name: str, model: "ProductOrm"
+        session: session_factory, model: "ProductOrm", name: str
     ):
+        """
+        Возвращает последнюю версию продукта по имени
+        :param session: Сессия обращения в БД
+        :param model: ProductOrm
+        :param name: Имя продукта
+        :return: инстанс ProductOrm
+        """
         return (
             session.query(model)
             .filter_by(name=name)
@@ -20,35 +27,76 @@ class ProductQueries:
 
     @staticmethod
     def first_product_by_id_query(
-        session: session_factory, product_id: int, model: "ProductOrm"
+        session: session_factory, model: "ProductOrm", product_id: int
     ):
+        """
+        Возвращает первый продукт по ID
+        :param session: Сессия обращения в БД
+        :param model: ProductOrm
+        :param product_id: ID продукта
+        :return : инстанс ProductOrm
+        """
         return session.query(model).filter_by(id=int(product_id)).first()
 
     @staticmethod
     def products_versions_to_upgrade_query(
-        session: session_factory, name: str, new_name: str, model: "ProductOrm"
+        session: session_factory, model: "ProductOrm", name: str, new_name: str
     ):
-        return session.query(model).filter_by(name=name).update({model.name: new_name})
+        """
+        Обновляет имя продукта по текущему имени
+        :param session: Сессия обращения в БД
+        :param model: ProductOrm
+        :param name: имя продукта
+        :param new_name: текущее имя продукта
+        :return : инстанс ProductOrm
+        """
+        if name != new_name:
+            return (
+                session.query(model).filter_by(name=name).update({model.name: new_name})
+            )
+        else:
+            return
 
     @staticmethod
     def products_by_name_archived_query(
-        session: session_factory, name: str, model: "ProductOrm"
+        session: session_factory, model: "ProductOrm", name: str
     ):
+        """
+        Архивирует продукт по имени
+        :param session: Сессия обращения в БД
+        :param model: ProductOrm
+        :param name: имя продукта
+        :return : инстанс ProductOrm
+        """
         return session.query(model).filter_by(name=name).update({model.archived: True})
 
 
 class CategoriesQueries:
     @staticmethod
     def existing_category_by_name_query(
-        session: session_factory, name: str, model: "CategoryOrm"
+        session: session_factory, model: "CategoryOrm", name: str
     ):
+        """
+        Возвращает первую категорию по имени
+        :param session: Сессия обращения в БД
+        :param model: CategoryOrm
+        :param name: имя категории
+        :return : инстанс CategoryOrm
+        """
         return session.query(model).filter_by(name=name).first()
 
     @staticmethod
     def category_by_id_query(
-        session: session_factory, category_id: int, model: "CategoryOrm"
+        session: session_factory, model: "CategoryOrm", category_id: str
     ):
-        return session.get(model, category_id)
+        """
+        Возвращает категорию по ID
+        :param session: Сессия обращения в БД
+        :param model: CategoryOrm
+        :param category_id: ID категории
+        :return : инстанс CategoryOrm
+        """
+        return session.get(model, int(category_id))
 
     @staticmethod
     def category_upgrade_query(
@@ -58,6 +106,15 @@ class CategoriesQueries:
         new_name: str | None,
         new_description: str | None,
     ):
+        """
+        Обновляет категорию по ID
+        :param session: Сессия обращения в БД
+        :param model: CategoryOrm
+        :param category_id: ID категории
+        :param new_name: новое имя категории
+        :param new_description: новое описание категории
+        :return : инстанс CategoryOrm
+        """
         if new_name and new_description:
             return (
                 session.query(model)
@@ -76,3 +133,5 @@ class CategoriesQueries:
                 .filter_by(id=int(category_id))
                 .update({model.description: new_description})
             )
+        else:
+            return

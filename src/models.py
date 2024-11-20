5d6a32a71fa4839b1652d1e0938099f05e0af5c7
vadim-pugachev <vadimp_91@mail.ru>
2024-11-20 20:06:46 +0300

    Fix

--- src/models.py
+++ src/models.py
@@ -26,7 +26,7 @@ class ProductOrm(Base):
     updated_at: Mapped[datetime.datetime] = mapped_column(
         server_default=text("TIMEZONE('utc',now())"), onupdate=datetime.datetime.utcnow
     )
-    archived: Mapped[bool] = mapped_column(default=False)
+    archived: Mapped[bool] = mapped_column(nullable=True)
 
     categories: Mapped[list["CategoryOrm"]] = relationship(
         secondary="category_product_m2m", back_populates="products"
@@ -38,7 +38,7 @@ class CategoryOrm(Base):
     id: Mapped[pk]
     name: Mapped[str255]
     description: Mapped[str255 | None]
-    archived: Mapped[bool] = mapped_column(default=False)
+    archived: Mapped[bool] = mapped_column(nullable=True)
 
     products: Mapped[list["ProductOrm"]] = relationship(
         secondary="category_product_m2m", back_populates="categories"

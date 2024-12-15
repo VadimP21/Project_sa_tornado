2b90160f6e78479a880af274ea6ed53955ec290d
vadvik <vpugacev94@gmail.com>
2024-12-15 23:51:55 +0300

    Fix router

--- src/routers.py
+++ src/routers.py
@@ -7,19 +7,4 @@ from handlers.product_handlers import *
 
 routers = [
     URLSpec(r"/product/", ProductHandlers),
-    URLSpec(r"/product", ProductHandlers),
-    # url(r"/product/update", ProductHandlers),
-    # (r"/product", CreateProductHandler),
-    # (r"/product-list", GetProductListHandler),
-    # (r"/product/update", UpdateProductHandler),
-    # (
-    #     r"/product/delete",
-    #     DeleteProductHandler,
-    # ),
-    # (r"/product/([^/]+)", GetProductHandler),
-    # (r"/category", CreateCategoryHandler),
-    # (r"/category-list", GetCategoryListHandler),
-    # (r"/category/update", UpdateCategoryHandler),
-    # (r"/category/delete", DeleteCategoryHandler),
-    # (r"/category/([^/]+)", GetCategoryHandler),
 ]

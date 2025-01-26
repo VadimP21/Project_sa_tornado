665b1ceb1fc7397ce4155b8c25e3952824862114
vadvik <vpugacev94@gmail.com>
2025-01-26 10:36:07 +0300

    Fix adn create ProductListHandler

--- src/handlers/product_handlers.py
+++ src/handlers/product_handlers.py
@@ -3,10 +3,7 @@
 """
 
 from tornado.web import RequestHandler
-
-from handlers.base_handlers import BaseHandler
 from services.product_service import ProductService
-from src.db_repository.product_repository import SyncORM
 
 
 class ProductHandlers(RequestHandler):
@@ -18,11 +15,9 @@ class ProductHandlers(RequestHandler):
         """
         Функция получения последней версии продукта по ID или имени
         """
-        product_id = self.get_query_argument("product_id", None)
-        name = self.get_query_argument("name", None)
         kwargs = {
-            "name": name,
-            "id": product_id,
+            "name": self.get_query_argument("product_id", None),
+            "id": self.get_query_argument("name", None),
         }
         result = ProductService.read_one(**kwargs)
         self.set_status(status_code=int(result["status_code"]))
@@ -57,7 +52,6 @@ class ProductHandlers(RequestHandler):
         """
         Функция софт-удаления продукта по ID или имени
         """
-        print("delete")
         kwargs = {
             "id": self.get_argument("product_id", None),
             "name": self.get_argument("name", None),
@@ -67,74 +61,28 @@ class ProductHandlers(RequestHandler):
         self.write(chunk=result["data"])
 
 
-class GetProductListHandler(BaseHandler):
+class ProductListHandler(RequestHandler):
     """
     Обработчик для получения списка всех продуктов
     """
 
     def get(self):
+        """
+        Функция получения списка продуктов
+        """
+
         page_number = self.get_argument("page_number", "1")
         page_size = self.get_argument("page_size", "20")
         sort_field = self.get_argument("sort_field", "id")
         sort_order = self.get_argument("sort_order", "asc")
         product_version = self.get_argument("product_version", "last")
-        self.base_request(
-            SyncORM.get_product_list,
-            page_number=page_number,
-            page_size=page_size,
-            sort_field=sort_field,
-            sort_order=sort_order,
-            product_version=product_version,
-        )
-
-
-class CreateProductHandler(BaseHandler):
-    """
-    Обработчик для создания продуктов
-    """
-
-    def post(self) -> None:
-        name = self.get_argument("name")
-        price = self.get_argument("price")
-        self.base_request(SyncORM.create_product, name=name, price=price)
-
-
-class GetProductHandler(RequestHandler):
-    """
-    Обработчик для получения списка продуктов по названию
-    """
-
-    def get(self):
-        pass
-
-
-class UpdateProductHandler(BaseHandler):
-    """
-    Обработчик для изменения продуктов
-    """
-
-    def post(self) -> None:
-        product_id = self.get_argument("id")
-        new_name = self.get_argument("new_name")
-        self.base_request(
-            SyncORM.upgrade_product, product_id=product_id, new_name=new_name
-        )
-
-
-class DeleteProductHandler(BaseHandler):
-    """
-    Обработчик для удаления продуктов
-    """
-
-    def post(self) -> None:
-        name = self.get_argument("name")
-        self.base_request(SyncORM.archive_product, name=name)
-
-
-class GetCategoryListHandler(RequestHandler):
-    """
-    Обработчик для получения списка всех элементов каталога
-    """
-
-    def get(self):
-        pass
+        kwargs = {
+            "page_number": page_number,
+            "page_size": page_size,
+            "sort_field": sort_field,
+            "sort_order": sort_order,
+            "product_version": product_version,
+        }
+        result = ProductService.read_one(**kwargs)
+        self.set_status(status_code=int(result["status_code"]))
+        self.write(chunk=result["data"])
